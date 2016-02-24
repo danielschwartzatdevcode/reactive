46588e2aa4115e1bcbfff291c7b4e1b3e250bb25
christopher.yamba <christopher.yamba@gmail.com>
2016-02-25 00:36:25 +0100

    simple impl of Observable.from and subsribe

--- christopher_yamba/src/main/java/com/devcode/reactive/App.java
+++ christopher_yamba/src/main/java/com/devcode/reactive/App.java
@@ -1,9 +1,34 @@
 package com.devcode.reactive;
 
+import rx.Observable;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 /**
  * @author Christopher Yamba
  */
 public class App {
+
+    private List<Author> authors;
+
+    public App(Author...authors) {
+        this.authors = Arrays.asList(authors);
+    }
+
+
+
+    public class Author{
+        String name;
+        List<String> post;
+
+
+        public Author(String name, String...post) {
+            this.name = name;
+            this.post = Arrays.asList(post);
+        }
+    }
 
     public static void main(String... args) {
 
--- christopher_yamba/src/test/java/com/devcode/reactive/AppTest.java
+++ christopher_yamba/src/test/java/com/devcode/reactive/AppTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 public class AppTest {
 
+
+
     @Test
     public void shouldInvokeXWithY(){
 
