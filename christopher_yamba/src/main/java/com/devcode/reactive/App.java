f891688a3132fae69881a8f9f2a3328f20bc23f5
christopher.yamba <christopher.yamba@gmail.com>
2016-02-24 22:40:34 +0100

    scaffold

---
+++ christopher_yamba/src/main/java/com/devcode/reactive/App.java
@@ -0,0 +1,11 @@
+package com.devcode.reactive;
+
+/**
+ * @author Christopher Yamba
+ */
+public class App {
+
+    public static void main(String... args) {
+
+    }
+}
---
+++ christopher_yamba/src/test/java/com/devcode/reactive/AppTest.java
@@ -0,0 +1,12 @@
+package com.devcode.reactive;
+
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldInvokeXWithY(){
+
+    }
+}
